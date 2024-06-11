22673395aa06e155d30c4020691d58ab013e74b3
daniel <me@danielnord.no>
2024-06-11 21:42:22 +0200

    Log received mqtt messages to application log.

--- src/main/kotlin/io/nordlab/legocity/server/system/mqtt/MqttSubscriber.kt
+++ src/main/kotlin/io/nordlab/legocity/server/system/mqtt/MqttSubscriber.kt
@@ -2,6 +2,9 @@ package io.nordlab.legocity.server.system.mqtt
 
 import io.micronaut.mqtt.annotation.MqttSubscriber
 import io.micronaut.mqtt.annotation.Topic
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 @MqttSubscriber
 class MqttSubscriber {
@@ -9,6 +12,7 @@ class MqttSubscriber {
 
     @Topic("legocity/#")
     fun receive(data: String) {
+        logger.info { "Received message: $data" }
         event = data
     }
 }
