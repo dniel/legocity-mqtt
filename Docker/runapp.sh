509a92d1d2ea7e8faafc56beb2763d49370126af
daniel <me@danielnord.no>
2024-06-14 10:30:51 +0200

    remove path to java

--- Docker/runapp.sh
+++ Docker/runapp.sh
@@ -8,12 +8,12 @@ echo " --- RUNNING $(basename "$0") $(date -u "+%Y-%m-%d %H:%M:%S Z") --- "
 set -x
 
 # Print some debug info about the JVM and Heap
-exec su-exec "$USER:$GROUP" "$JAVA_HOME/bin/java" \
+exec su-exec "$USER:$GROUP" "java" \
   -XX:MaxRAMPercentage=80 \
   -XX:+PrintFlagsFinal \
   -version | grep Heap
 
-exec su-exec "$USER:$GROUP" "$JAVA_HOME/bin/java" \
+exec su-exec "$USER:$GROUP" "java" \
   -XX:MaxRAMPercentage=80 \
   -Djava.util.concurrent.ForkJoinPool.common.parallelism=4 \
   -jar "$APP_JAR"
